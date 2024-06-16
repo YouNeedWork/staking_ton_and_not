ce91e7cd8ab00a063f0da4690489f8031344a88f
YusongWang <admin@wangyusong.com>
2024-06-16 10:48:33 -0800

    fix deploy

--- scripts/deployStake.ts
+++ scripts/deployStake.ts
@@ -1,4 +1,4 @@
-import { toNano } from '@ton/core';
+import { Address, Cell, address, toNano } from '@ton/core';
 import { Stake } from '../wrappers/Stake';
 import { compile, NetworkProvider } from '@ton/blueprint';
 
@@ -6,16 +6,17 @@ export async function run(provider: NetworkProvider) {
     const stake = provider.open(
         Stake.createFromConfig(
             {
-                id: Math.floor(Math.random() * 10000),
-                counter: 0,
+                lock_time: 1800,
+                not_addr: address('UQDtU7yZnlpK9po_nD3lN299kMSH4VKPMx5xbb6FkD1REpyy'),
+                not_bytecode: Cell.fromBase64(
+                    'te6ccgEBAQEAcQAA3v8AIN0gggFMl7ohggEznLqxn3Gw7UTQ0x/THzHXC//jBOCk8mCDCNcYINMf0x/TH/gjE7vyY+1E0NMf0x/T/9FRMrryoVFEuvKiBPkBVBBV+RDyo/gAkyDXSpbTB9QC+wDo0QGkyMsfyx/L/8ntVA==',
+                ),
+                admin_addr: address('UQAIehKxdz_4OjYcXyN7X_i6CsKZGF3SsikO_VqS9wikEmA4'),
             },
-            await compile('Stake')
-        )
+            await compile('Stake'),
+        ),
     );
 
     await stake.sendDeploy(provider.sender(), toNano('0.05'));
-
     await provider.waitForDeploy(stake.address);
-
-    console.log('ID', await stake.getID());
 }
--- wrappers/Stake.ts
+++ wrappers/Stake.ts
@@ -1,12 +1,20 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
 export type StakeConfig = {
-    id: number;
-    counter: number;
+    lock_time: number;
+    not_addr: Address;
+    not_bytecode: Cell;
+    admin_addr: Address;
 };
 
 export function stakeConfigToCell(config: StakeConfig): Cell {
-    return beginCell().storeUint(config.id, 32).storeUint(config.counter, 32).endCell();
+    return beginCell()
+        .storeUint(config.lock_time, 32)
+        .storeRef(beginCell().storeAddress(config.not_addr).storeRef(config.not_bytecode).endCell())
+        .storeRef(beginCell().storeAddress(config.admin_addr).endCell())
+        .storeDict(null)
+        .storeDict(null)
+        .endCell();
 }
 
 export const Opcodes = {
@@ -14,7 +22,10 @@ export const Opcodes = {
 };
 
 export class Stake implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+    constructor(
+        readonly address: Address,
+        readonly init?: { code: Cell; data: Cell },
+    ) {}
 
     static createFromAddress(address: Address) {
         return new Stake(address);
@@ -32,35 +43,5 @@ export class Stake implements Contract {
             sendMode: SendMode.PAY_GAS_SEPARATELY,
             body: beginCell().endCell(),
         });
-    }
-
-    async sendIncrease(
-        provider: ContractProvider,
-        via: Sender,
-        opts: {
-            increaseBy: number;
-            value: bigint;
-            queryID?: number;
-        }
-    ) {
-        await provider.internal(via, {
-            value: opts.value,
-            sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell()
-                .storeUint(Opcodes.increase, 32)
-                .storeUint(opts.queryID ?? 0, 64)
-                .storeUint(opts.increaseBy, 32)
-                .endCell(),
-        });
-    }
-
-    async getCounter(provider: ContractProvider) {
-        const result = await provider.get('get_counter', []);
-        return result.stack.readNumber();
-    }
-
-    async getID(provider: ContractProvider) {
-        const result = await provider.get('get_id', []);
-        return result.stack.readNumber();
     }
 }
